b51853a3a291be7d0e2fce585fd4b9fe4f569881
erwinvandenbeld <erwin.van.den.beld@topicus.nl>
2021-04-16 21:52:33 +0200

    Update project version upon releasing fix

--- project.clj
+++ project.clj
@@ -8,7 +8,7 @@
             :key "mit"}
   :dependencies [[org.clojure/clojure "1.10.1"], [clj-jgit "1.0.1"], [com.fzakaria/slf4j-timbre "0.3.21"], [cli-matic "0.4.3"]]
   :plugins [[lein-cloverage "1.2.2"], [lein-kibit "0.1.8"], [lein-set-version "0.4.1"]]
-  :repositories [["github" {:url "https://maven.pkg.github.com/erwinvandenbeld/four-key-cli" :username :env/GITHUB_ACTOR :password :env/GITHUB_TOKEN}]]
+  :repositories [["github" {:sign-releases false :url "https://maven.pkg.github.com/erwinvandenbeld/four-key-cli" :username :env/GITHUB_ACTOR :password :env/GITHUB_TOKEN}]]
   :main four-key-cli.core
   :aot [four-key-cli.core]
   :repl-options {:init-ns four-key-cli.core})
