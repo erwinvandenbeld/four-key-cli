48d2e56bf7cb17f25ba7af8d640a64136846c2b7
erwinvandenbeld <erwin.van.den.beld@topicus.nl>
2021-04-16 21:47:56 +0200

    Update project version upon releasing

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # Four Key Cli
 
 ![Clojure CI](https://github.com/erwinvandenbeld/four-key-cli/actions/workflows/clojure.yml/badge.svg "Clojure CI") ![Clojure CI](https://github.com/erwinvandenbeld/four-key-cli/actions/workflows/release-package.yml/badge.svg "Clojure CI")
+<br/>
 Command Line Interface (CLI) to extract [four-key-metrics](https://www.thoughtworks.com/radar/techniques/four-key-metrics) from a GIT repository.
 
 ## Usage
--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject four-key-cli "0.1.0-SNAPSHOT"
+(defproject four-key-cli "1.0.0-SNAPSHOT"
   :description "Command Line Interface (CLI) to extract four-key-metrics"
   :url "https://github.com/erwinvandenbeld/four-key-cli"
   :license {:name "MIT"
@@ -7,7 +7,7 @@
             :year 2021
             :key "mit"}
   :dependencies [[org.clojure/clojure "1.10.1"], [clj-jgit "1.0.1"], [com.fzakaria/slf4j-timbre "0.3.21"], [cli-matic "0.4.3"]]
-  :plugins [[lein-cloverage "1.2.2"], [lein-kibit "0.1.8"]]
+  :plugins [[lein-cloverage "1.2.2"], [lein-kibit "0.1.8"], [lein-set-version "0.4.1"]]
   :repositories [["github" {:url "https://maven.pkg.github.com/erwinvandenbeld/four-key-cli" :username :env/GITHUB_ACTOR :password :env/GITHUB_TOKEN}]]
   :main four-key-cli.core
   :aot [four-key-cli.core]
